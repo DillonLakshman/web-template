9d73a1d3f15d5a185619f9627296e769b0327389
ngunawardena <ngunawardena@virtusa.com>
2018-11-06 17:19:53 +0530

    Nigels components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CardsFullSectionComponent } from './cards-full-section/cards-full-section.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BodySectionComponent } from './body-section/body-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardsFullSectionComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    BodySectionComponent
   ],
   imports: [
     BrowserModule,
